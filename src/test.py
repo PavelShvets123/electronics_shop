58ae62c84d381bdbbd38c2f1bcd86791ba8b6b82
PavelShvets123 <pasha.shvets99@mail.ru>
2023-06-28 11:28:52 +0300

    Delete test.py

--- src/test.py
+++
@@ -1,4 +0,0 @@
-with open('..\src\items.csv', 'r', encoding='windows-1251') as file:
-    reader = csv.DictReader(file, delimiter=',')
-    for row in reader:
-        print(row['name'], int(row['price']), int(row['quantity']))
